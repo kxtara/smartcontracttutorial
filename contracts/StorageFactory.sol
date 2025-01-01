9af35fe5e773c661f2e02bea23d4c3d6d590517b
kxtara <kiarahoheb@gmail.com>
2025-01-01 15:57:22 -0400

    Deploy SimpleStorage from StorageFactory contract

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -3,6 +3,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity  ^0.6.0;
 
+import "contracts/SimpleStorage.sol";
+
+// contract to deploy SimpleStorage contract
 contract StorageFactory {
-    
+
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage(); 
+        // create an object of type SimpleStorage
+        simpleStorageArray.push();(simpleStorage);
+        
+    }
 }
